e2bd1301adaa587e7c6da53d40d455c6ef78cd2c
Danrognoni <131401219+Danrognoni@users.noreply.github.com>
2025-10-30 15:39:49 -0300

    necessary things

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
+
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { ProductoComponent } from './pages/producto/producto.component';
+import { ProveedorComponent } from './pages/proveedor/proveedor.component';
+import { ExistenciaComponent } from './pages/existencia/existencia.component';
+import { DescuentoComponent } from './pages/descuento/descuento.component';
+
+
+export const routes: Routes = [
+  {path :'', component:HomeComponent},
+  { path: 'productos', component:ProductoComponent },
+   { path: 'proveedores', component:ProveedorComponent },
+   { path: 'existencia', component:ExistenciaComponent },
+   {path:'descuento', component:DescuentoComponent}
+];
---
+++ src/app/pages/existencia/existencia.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-existencia',
+  imports: [],
+  templateUrl: './existencia.component.html',
+  styleUrl: './existencia.component.css'
+})
+export class ExistenciaComponent {
+
+}
