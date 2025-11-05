f3020b3a89078b9af4c74996281fee41ac52ebea
Gianluca Castorina <58142912+gianmdp03@users.noreply.github.com>
2025-11-05 16:47:14 -0300

    LINK PARA VOLVER

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLinkWithHref } from '@angular/router';
 import { NavbarComponent } from "./components/navbar/navbar/navbar.component";
 import { ProductoComponent } from './pages/producto-list/producto.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLinkWithHref],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
