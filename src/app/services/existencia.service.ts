e02bf7edadc4322755988940e9b8041a8e136114
Joaquin <joaco.rodriguezortega@gmail.com>
2025-11-05 17:56:11 -0300

    existenciaService ts

--- src/app/services/existencia.service.ts
+++ src/app/services/existencia.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Existencia } from '../model/existencia';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExistenciaService {
+  readonly apiUrl='http://localhost:3000/existencias'
+  existencias: Existencia[];
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+      this.existencias=[];
+   }
+
+  getExistencias(){
+    return this.http.get<Existencia[]>(this.apiUrl);
+  }
+
+  postExistencias(data: Omit<Existencia, 'id'>): Observable<Existencia>{
+    return this.http.post<Existencia>(this.apiUrl, data);
+  }
 }
