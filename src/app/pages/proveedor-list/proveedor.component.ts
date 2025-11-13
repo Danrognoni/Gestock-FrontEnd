95b76898adff95c0d4e8507fb115056ff7487a9e
Joaquin <joaco.rodriguezortega@gmail.com>
2025-11-13 20:39:44 -0300

    proveedor ts fixed

--- src/app/pages/proveedor-list/proveedor.component.ts
+++ src/app/pages/proveedor-list/proveedor.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { ProveedorService } from '../../services/proveedor.service';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { Proveedor } from '../../model/proveedor';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-proveedor',
@@ -11,15 +12,13 @@ import { Proveedor } from '../../model/proveedor';
 })
 export class ProveedorComponent {
   private proveedorService = inject(ProveedorService);
+  private router = inject(Router);
   public proveedores = toSignal(this.proveedorService.getProveedores(), {
     initialValue: []
   });
   public proveedorSeleccionado:Proveedor | null = null;
 
-  verDetalle(proveedor: Proveedor){
-    this.proveedorSeleccionado = proveedor;
-  }
-  cerrarDetalle(){
-    this.proveedorSeleccionado = null;
+  proveedorDetail(id: number){
+    this.router.navigate(['proveedores/proveedorDetail/', id]);
   }
 }
